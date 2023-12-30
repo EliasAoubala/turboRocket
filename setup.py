3cb86853ec70b337c14ac8d16e1147e7d0995806
Elias Aoubala <2432537A@student.gla.ac.uk>
2023-12-30 14:55:01 +0000

    Setup main python library structure

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A turbomachinery design package.'
+LONG_DESCRIPTION = 'A tool-set package for the design of turbomachinery components, for use in liquid rocket engine design.'
+
+# Setting up
+setup(
+    name="turboRocket",
+    version=VERSION,
+    author="Elias Aoubala (Elias Aoubala)",
+    author_email="<elias.aoubala@outlook.com>",
+    license="MIT",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[''],
+    keywords=['python', 'rocket propulsion', 'LRE', 'turbomachinery'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
